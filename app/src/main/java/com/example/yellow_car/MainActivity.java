57b3911e03311c2a3192e244d72670551cf87f9f
superTimi <1044800346@qq.com>
2019-05-30 09:18:09 +0800

    加载webview

--- app/src/main/java/com/example/yellow_car/MainActivity.java
+++ app/src/main/java/com/example/yellow_car/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.yellow_car;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:////android_asset/demo.html");
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("javascript: setBox()");
+            }
+        });
+
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient(){
+
+        });
     }
 }
